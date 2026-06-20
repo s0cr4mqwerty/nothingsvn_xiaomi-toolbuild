26c1ea299d8355b7723ea7fc0971831535ce5a7e
Chau Vinh Tien <tiencv102006@gmail.com>
2026-06-20 10:01:14 +0700

    bye theme default

--- bin/modfile/UpdateFile/Global_POCO2Xiaomi-Icon/update.sh
+++ bin/modfile/UpdateFile/Global_POCO2Xiaomi-Icon/update.sh
@@ -5,15 +5,10 @@ regionTYPE=$(cat $work_dir/bin/ddevice/device_type.txt)
 rom_os=$(cat $work_dir/bin/ddevice/rom_os.txt)
 
 if [[ $regionTYPE == *"Global"* ]]; then
-    if [[ $rom_os == "OS3" ]]; then
-        mods "Disable POCO Theme OS3"
-        rm -rf "$product/media/theme"
-        cp -rf "$work_dir/bin/modfile/UpdateFile/Global_POCO2Xiaomi-Icon/OS3/theme" "$product/media"
-        mods "Done"
-    elif [[ $rom_os == "OS2" || $rom_os == "OS1" ]]; then
-        mods "Disable POCO Theme OS2 Below"
-        rm -rf "$product/media/theme"
-        cp -rf "$work_dir/bin/modfile/UpdateFile/Global_POCO2Xiaomi-Icon/OS2BL/theme" "$product/media"
-        mods "Done"
-    fi
+    mods "Disable POCO Theme"
+    rm -rf "$product/media/theme/default/icons"
+    rm -rf "$product/media/theme/default/dynamicicons"s
+    cp -rf "$work_dir/bin/modfile/UpdateFile/Global_POCO2Xiaomi-Icon/icons" "$product/media/theme/default"
+    cp -rf "$work_dir/bin/modfile/UpdateFile/Global_POCO2Xiaomi-Icon/dynamicicons" "$product/media/theme/default"
+    mods "Done"
 fi
